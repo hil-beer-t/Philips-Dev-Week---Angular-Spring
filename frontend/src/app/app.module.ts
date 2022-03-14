a35f23e360dd5f83c2c2e871b243196fb7b60453
hil-beer-t <hil.beer.t.git@gmail.com>
2022-03-13 23:10:44 -0300

    add frontend integration

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,11 +1,12 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
-
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -14,8 +15,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     MatToolbarModule,
-    BrowserAnimationsModule
+    MatTableModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/ocorrencias/service/faixa-etaria.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Faixaetaria } from '../model/faixaetaria';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FaixaEtariaService {
+
+  constructor(private http:HttpClient) { }
+
+  listFaixaEtaria(): Observable<Faixaetaria[] >{
+    const caminhourl = '/api/faixaetaria';
+
+    return this.http.get<Faixaetaria[]>(caminhourl);
+
+  }
+}
